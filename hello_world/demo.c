1852357a2686f1e73b7ab06546dd6664e903adc0
zhonghao.0930 <zhonghao.0930@bytedance.com>
2024-05-01 20:55:34 +0800

    chore: update demo.c

--- hello_world/demo.c
+++ hello_world/demo.c
@@ -1,8 +1,21 @@
 #include <avformat.h>
 #include <stdio.h>
 
+// print out the steps and errors
+static void logging(const char *fmt, ...);
 
 int main() {
-  printf("Hello, World!\n");
+  logging("initializing all the containers, codecs and protocols.");
+
   return 0;
+}
+
+static void logging(const char *fmt, ...)
+{
+    va_list args;
+    fprintf( stderr, "LOG: " );
+    va_start( args, fmt );
+    vfprintf( stderr, fmt, args );
+    va_end( args );
+    fprintf( stderr, "\n" );
 }
