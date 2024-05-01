9865e3649a65db36b0fea673b80652ecb5df3332
zhonghao.0930 <zhonghao.0930@bytedance.com>
2024-05-01 21:50:15 +0800

    chore: update demo.c

--- hello_world/demo.c
+++ hello_world/demo.c
@@ -5,7 +5,17 @@
 static void logging(const char *fmt, ...);
 
 int main() {
+
   logging("initializing all the containers, codecs and protocols.");
+
+  AvFormatContext *pFormatContext = avformat_alloc_context();
+
+  if (!pFormatContext) {
+    logging("ERROR could not allocate memory for Format Context");
+    return -1;
+  }
+
+  logging("opening the input file (%s) and loading format (container) header", argv[1]);
 
   return 0;
 }
